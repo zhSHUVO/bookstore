b1a300d1ae13c5207c799f137342d07627f5e565
zhshuvo <zhshuvo99@gmail.com>
2023-03-17 11:20:00 +0600

    add books

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -5,11 +5,21 @@ export const apiSlice = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: "http://localhost:9000",
     }),
+    tagTypes: ["Books"],
     endpoints: (builder) => ({
         getBooks: builder.query({
             query: () => "/books",
+            providesTags: ["Books"],
+        }),
+        addBook: builder.mutation({
+            query: (data) => ({
+                url: "/books",
+                method: "POST",
+                body: data,
+            }),
+            invalidatesTags: ["Books"],
         }),
     }),
 });
 
-export const { useGetBooksQuery } = apiSlice;
+export const { useGetBooksQuery, useAddBookMutation } = apiSlice;
--- src/pages/Add.js
+++ src/pages/Add.js
@@ -1,102 +1,130 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAddBookMutation } from "../features/api/apiSlice";
 
 function Add(props) {
+    const [addBook, {isLoading}] = useAddBookMutation();
+    const navigate = useNavigate();
+
+    const [name, setName] = useState("");
+    const [author, setAuthor] = useState("");
+    const [thumbnail, setThumbnail] = useState("");
+    const [price, setPrice] = useState(0);
+    const [rating, setRating] = useState(0);
+    const [featured, setFeatured] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addBook({ name, author, thumbnail, price, rating, featured });
+
+        navigate("/");
+    };
     return (
-        <div>
-            <main className="py-6 2xl:px-6">
-                <div className="container">
-                    <div className="p-8 overflow-hidden bg-white shadow-cardShadow rounded-md max-w-xl mx-auto">
-                        <h4 className="mb-8 text-xl font-bold text-center">
-                            Add New Book
-                        </h4>
-                        <form className="book-form">
+        <main className="py-6 2xl:px-6">
+            <div className="container">
+                <div className="p-8 overflow-hidden bg-white shadow-cardShadow rounded-md max-w-xl mx-auto">
+                    <h4 className="mb-8 text-xl font-bold text-center">
+                        Add New Book
+                    </h4>
+                    <form
+                        className="book-form"
+                        onSubmit={(e) => handleSubmit(e)}
+                    >
+                        <div className="space-y-2">
+                            <label>Book Name</label>
+                            <input
+                                required
+                                className="text-input"
+                                type="text"
+                                id="lws-bookName"
+                                name="name"
+                                onChange={(e) => setName(e.target.value)}
+                            />
+                        </div>
+
+                        <div className="space-y-2">
+                            <label>Author</label>
+                            <input
+                                required
+                                className="text-input"
+                                type="text"
+                                id="lws-author"
+                                name="author"
+                                onChange={(e) => setAuthor(e.target.value)}
+                            />
+                        </div>
+
+                        <div className="space-y-2">
+                            <label>Image Url</label>
+                            <input
+                                required
+                                className="text-input"
+                                type="text"
+                                id="lws-thumbnail"
+                                name="thumbnail"
+                                onChange={(e) => setThumbnail(e.target.value)}
+                            />
+                        </div>
+
+                        <div className="grid grid-cols-2 gap-8 pb-4">
                             <div className="space-y-2">
-                                <label htmlFor="lws-bookName">Book Name</label>
+                                <label>Price</label>
                                 <input
                                     required
                                     className="text-input"
-                                    type="text"
-                                    id="lws-bookName"
-                                    name="name"
+                                    type="number"
+                                    id="lws-price"
+                                    name="price"
+                                    step="0.01"
+                                    onChange={(e) =>
+                                        setPrice(parseFloat(e.target.value))
+                                    }
                                 />
                             </div>
 
                             <div className="space-y-2">
-                                <label htmlFor="lws-author">Author</label>
+                                <label>Rating</label>
                                 <input
                                     required
                                     className="text-input"
-                                    type="text"
-                                    id="lws-author"
-                                    name="author"
+                                    type="number"
+                                    id="lws-rating"
+                                    name="rating"
+                                    min="1"
+                                    max="5"
+                                    onChange={(e) =>
+                                        setRating(parseInt(e.target.value))
+                                    }
                                 />
                             </div>
+                        </div>
 
-                            <div className="space-y-2">
-                                <label htmlFor="lws-thumbnail">Image Url</label>
-                                <input
-                                    required
-                                    className="text-input"
-                                    type="text"
-                                    id="lws-thumbnail"
-                                    name="thumbnail"
-                                />
-                            </div>
+                        <div className="flex items-center">
+                            <input
+                                id="lws-featured"
+                                type="checkbox"
+                                name="featured"
+                                className="w-4 h-4"
+                                onClick={(e) => setFeatured(e.target.checked)}
+                            />
+                            <label className="ml-2 text-sm">
+                                {" "}
+                                This is a featured book{" "}
+                            </label>
+                        </div>
 
-                            <div className="grid grid-cols-2 gap-8 pb-4">
-                                <div className="space-y-2">
-                                    <label htmlFor="lws-price">Price</label>
-                                    <input
-                                        required
-                                        className="text-input"
-                                        type="number"
-                                        id="lws-price"
-                                        name="price"
-                                    />
-                                </div>
-
-                                <div className="space-y-2">
-                                    <label htmlFor="lws-rating">Rating</label>
-                                    <input
-                                        required
-                                        className="text-input"
-                                        type="number"
-                                        id="lws-rating"
-                                        name="rating"
-                                        min="1"
-                                        max="5"
-                                    />
-                                </div>
-                            </div>
-
-                            <div className="flex items-center">
-                                <input
-                                    id="lws-featured"
-                                    type="checkbox"
-                                    name="featured"
-                                    className="w-4 h-4"
-                                />
-                                <label
-                                    htmlFor="lws-featured"
-                                    className="ml-2 text-sm"
-                                >
-                                    {" "}
-                                    This is a featured book{" "}
-                                </label>
-                            </div>
-
-                            <button
-                                type="submit"
-                                className="submit"
-                                id="lws-submit"
-                            >
-                                Add Book
-                            </button>
-                        </form>
-                    </div>
+                        <button
+                            disabled={isLoading}
+                            type="submit"
+                            className="submit"
+                            id="lws-submit"
+                        >
+                            Add Book
+                        </button>
+                    </form>
                 </div>
-            </main>
-        </div>
+            </div>
+        </main>
     );
 }
 
